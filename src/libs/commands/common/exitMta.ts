1659d5bdbb5933e9c0042078866f391837ce8dd6
EnergyPatrikHU <energyhun24@gmail.com>
2023-10-28 00:44:25 +0200

    fix 'exitmta' command

--- src/libs/commands/common/exitMta.ts
+++ src/libs/commands/common/exitMta.ts
@@ -8,7 +8,7 @@ import texts from '../../texts';
 cmd.registerCommand(
 	async (speechSynthesis: SpeechSynthesisConfig) => {
 		try {
-			if (['gta_sa.exe'].includes(await getActiveWindowName())) {
+			if (!['gta_sa.exe', 'proxy_sa.exe'].includes(await getActiveWindowName())) {
 				logLine(texts().textFeedback.commands.exitMta.notInForeground);
 				speechSynthesis(texts().speechFeedback.commands.exitMta.notInForeground);
 				return;
@@ -19,5 +19,5 @@ cmd.registerCommand(
 			logLine(error);
 		}
 	},
-	['both', 'MTA:SA Bezárás', '!mtabezárás | !mtakilépés', ['mtabezárás', 'mtakilépés'], texts().textFeedback.commands.exitMta.description],
+	['both', 'MTA:SA Bezárás', null, ['mtabezárás', 'mtakilépés', 'closemta', 'exitmta'], texts().textFeedback.commands.exitMta.description],
 );
